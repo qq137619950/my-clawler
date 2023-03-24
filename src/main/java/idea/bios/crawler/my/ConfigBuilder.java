fbb795a4093613deb6a0adcef6b717d284becd91
guiweikang <guiweikang@idea.edu.cn>
2023-03-24 11:26:49 +0800

    重构，去除多余组件

--- src/main/java/idea/bios/crawler/my/ConfigBuilder.java
+++ src/main/java/idea/bios/crawler/my/ConfigBuilder.java
@@ -16,7 +16,7 @@ public class ConfigBuilder {
 
     public static RobotsTxtConfig defaultRobotsBuilder() {
         var robotsTxtConfig = new RobotsTxtConfig();
-        robotsTxtConfig.setEnabled(false);
+        // robotsTxtConfig.setEnabled(false);
         robotsTxtConfig.setUserAgentName("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8");
         return robotsTxtConfig;
     }
