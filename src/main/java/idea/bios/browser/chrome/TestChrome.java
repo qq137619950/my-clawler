bdf4984635ff4a75302b597fe70b3d7706728e84
guiweikang <guiweikang@idea.edu.cn>
2023-03-29 11:25:37 +0800

    增加 RedditCommentCrawler

--- src/main/java/idea/bios/browser/chrome/TestChrome.java
+++ src/main/java/idea/bios/browser/chrome/TestChrome.java
@@ -1,26 +1,20 @@
 package idea.bios.browser.chrome;
 
 import com.google.gson.Gson;
-import idea.bios.util.selenium.SeleniumBuilder;
+import com.mongodb.client.MongoCollection;
+import idea.bios.datasource.mongodb.MongoDb;
 import idea.bios.util.selenium.script.ChromeDriverBuilder;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import lombok.var;
+import org.bson.Document;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.io.FileReader;
+import java.util.*;
 
 /**
  * Chrome引擎测试
@@ -28,20 +22,35 @@ import java.util.stream.IntStream;
  */
 @Slf4j
 public class TestChrome {
-    private static final String URL =
-            "https://www.reddit.com/r/CryptoCurrency/comments/122f6cq/exactly_10_years_ago_the_cyprus_government/";
+    private static final MongoCollection<Document> COLLECTION = new MongoDb()
+            .getCrawlerDataCollection("com.reddit.comment");
 
     public static void main(String[] args) throws InterruptedException {
+        try (var sc = new Scanner(new FileReader("C:/crawler/reditt.txt"))) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (!line.isEmpty()) {
+                    run(line);
+                }
+            }
+        } catch (Exception ignored) {
+            log.warn("Exception", ignored);
+        }
+    }
+
+    private static void run(String url) throws InterruptedException {
+        log.warn("________________URL:{}", url);
         var result = new LinkedHashMap<String, Object>();
         ChromeDriver driver = null;
         // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         List<WebElement> dataParentElement = new ArrayList<>();
         while (dataParentElement.isEmpty()) {
             if (driver != null) {
+                driver.quit();
                 driver.close();
             }
             driver = ChromeDriverBuilder.buildScriptChromeDriver();
-            driver.get(URL);
+            driver.get(url);
             Thread.sleep(10000);
             dataParentElement = driver.findElements(By.xpath(
                     "//div[@data-scroller-first]/parent::div"));
@@ -109,9 +118,9 @@ public class TestChrome {
         });
         // 处理level3
         ChromeDriver finalDriver = driver;
-        level3Map.forEach((curDao, url) -> {
+        level3Map.forEach((curDao, level3Url) -> {
             try {
-                finalDriver.get(url);
+                finalDriver.get(level3Url);
                 Thread.sleep(5000);
                 // 总体是level 3，最多3层级
                 List<WebElement> parent = finalDriver.findElements(By.xpath(
@@ -127,10 +136,16 @@ public class TestChrome {
                 e.printStackTrace();
             }
         });
-        result.put("commentList", contentList);
+        result.put("commentList", new Gson().toJson(contentList));
+        result.put("site", url);
         System.out.println("---------------------------------------------");
         System.out.println(new Gson().toJson(result));
+        // 落库
+        var doc = new Document();
+        result.forEach(doc::append);
+        COLLECTION.insertOne(doc);
     }
+
 
     private static DataDao buildTextDataDao(WebElement element) {
         List<WebElement> contentElement = element.findElements(By.cssSelector(
