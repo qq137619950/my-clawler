04d1d2de64ce4d11c3ccec6d84528b0c13058b3e
Andy <1910620290@qq.com>
2023-04-04 17:52:08 +0800

    异常时候线程Sleep，转让CPU占用，防止有问题线程一直占用CPU

--- src/main/java/idea/bios/util/selenium/script/OpenReviewDriverCrawler.java
+++ src/main/java/idea/bios/util/selenium/script/OpenReviewDriverCrawler.java
@@ -5,18 +5,18 @@ import com.mongodb.client.MongoCollection;
 import idea.bios.datasource.mongodb.MongoDb;
 import lombok.extern.slf4j.Slf4j;
 import lombok.var;
+import org.apache.http.HttpHost;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.bson.Document;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -28,9 +28,10 @@ import java.util.stream.Collectors;
 @Slf4j
 public class OpenReviewDriverCrawler {
     private static final MongoCollection<Document> COLLECTION = new MongoDb()
-            .getCrawlerDataCollection("net.openreview.pdf");
+            .getCrawlerDataCollection("net.openreview.pdf.json");
 
     public static void main(String[] args) throws InterruptedException, IOException {
+
         // 查询所有groupId
         final String parentGroup = "ICLR.cc";
         CloseableHttpClient client = HttpClients.createDefault();
@@ -46,6 +47,9 @@ public class OpenReviewDriverCrawler {
         notes.forEach(item -> {
             String id = (String) item.get("id");
             String web = (String) item.get("web");
+            if (web == null) {
+                return;
+            }
             Pattern pattern = Pattern.compile(id + "/[a-zA-z0-9]*'");
             Matcher matcher = pattern.matcher(web);
             while (matcher.find()) {
@@ -64,11 +68,13 @@ public class OpenReviewDriverCrawler {
     }
 
     private static void run(String groupId) throws IOException {
-        var myRes = new ArrayList<Map<String, Object>>();
         final int step = 1000;
         int start = 0;
         // 调接口获取json
-        CloseableHttpClient client = HttpClients.createDefault();
+        HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+        var proxy = new HttpHost("192.168.218.37", 13128);
+        clientBuilder.setProxy(proxy);
+        CloseableHttpClient client = clientBuilder.build();
         var arr = new ArrayList<Map<String, Object>>();
         while (true) {
             var httpGet = new HttpGet(
@@ -92,7 +98,8 @@ public class OpenReviewDriverCrawler {
         arr.forEach(a -> {
             var finalItem = new LinkedHashMap<String, Object>();
             // id
-            finalItem.put("id", a.get("id"));
+            String dataId = (String) a.get("id");
+            finalItem.put("id", dataId);
             // signatures
             finalItem.put("signatures", a.get("signatures"));
             // content
@@ -108,7 +115,7 @@ public class OpenReviewDriverCrawler {
             finalItem.put("pdf", content.get("pdf"));
             // 查找详情页
             try {
-                Thread.sleep(1000);
+                Thread.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -125,23 +132,43 @@ public class OpenReviewDriverCrawler {
                 List<Map<String, Object>> notes = (List<Map<String, Object>>) resMap.get("notes");
                 notes.forEach(note -> {
                     var map = new LinkedHashMap<String, Object>();
-                    map.put("invitation", note.get("invitation"));
+                    String id = (String) note.get("id");
+                    if (id.equals(dataId)) {
+                        return;
+                    }
+                    map.put("id", note.get("id"));
+                    map.put("parentId", note.get("replyto"));
+                    String invitation = (String) note.get("invitation");
+                    if (invitation != null) {
+                        String[] strings = invitation.split("/");
+                        map.put("type", strings[strings.length - 1]);
+                    }
                     map.put("writers", note.get("writers"));
                     map.put("content", note.get("content"));
+                    // 时间
+                    var simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                    Double createTimeDb = (Double) note.get("cdate");
+                    Double modifyTimeDb = (Double) note.get("mdate");
+                    if (createTimeDb != null) {
+                        map.put("createTime", simpleDateFormat.format(new Date(createTimeDb.longValue())));
+                    }
+                    if (modifyTimeDb != null) {
+                        map.put("modifyTime", simpleDateFormat.format(new Date(modifyTimeDb.longValue())));
+                    }
                     finalNoteList.add(map);
                 });
             } catch (IOException e) {
                 e.printStackTrace();
             }
             finalItem.put("replies", finalNoteList);
-            myRes.add(finalItem);
+            try {
+                var document = new Document();
+                finalItem.forEach(document::append);
+                COLLECTION.insertOne(document);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         });
-        // 写入mongodb
-        COLLECTION.insertMany(myRes.stream().map(m -> {
-            var document = new Document();
-            m.forEach(document::append);
-            return document;
-        }).collect(Collectors.toList()));
     }
 
 }
