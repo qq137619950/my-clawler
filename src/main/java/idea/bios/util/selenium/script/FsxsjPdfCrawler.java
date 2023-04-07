4f986da419fce607cbc030cbe161ad1d1a5231af
guiweikang <guiweikang@idea.edu.cn>
2023-04-07 14:43:59 +0800

    增加医疗pdf获取

--- src/main/java/idea/bios/util/selenium/script/FsxsjPdfCrawler.java
+++ src/main/java/idea/bios/util/selenium/script/FsxsjPdfCrawler.java
@@ -11,14 +11,17 @@ import java.util.stream.IntStream;
 import static idea.bios.util.selenium.script.ChromeDriverBuilder.buildScriptChromeDriver;
 
 /**
+ * 列表：http://www.e-tiller.com/et/customer/list
  * http://www.fsxsj.net/ch/reader/issue_list.aspx?year_id=2023&quarter_id=01
  * http://www.spinejournal.net/ch/reader/issue_list.aspx?year_id=2022&quarter_id=1
  * 医学信息学杂志 http://www.yxxxx.ac.cn/ch/reader/issue_list.aspx?year_id=1997&quarter_id=1
+ * 中国卫生政策研究 http://journal.healthpolicy.cn/ch/reader/issue_list.aspx?year_id=2008&quarter_id=1
+ *
  * @author 86153
  */
 public class FsxsjPdfCrawler {
     private static final String MENU_SITE_PRE =
-            "http://www.yxxxx.ac.cn/ch/reader/issue_list.aspx?year_id=";
+            "http://journal.healthpolicy.cn/ch/reader/issue_list.aspx?year_id=";
 
     public static void main(String[] args) throws InterruptedException {
         run();
@@ -28,7 +31,7 @@ public class FsxsjPdfCrawler {
         ChromeDriver driver = buildScriptChromeDriver();
         driver.get("chrome://settings/content/pdfDocuments");
         Thread.sleep(5000);
-        IntStream.rangeClosed(2016, 2023).forEach(year -> IntStream.rangeClosed(1, 12)
+        IntStream.rangeClosed(2022, 2023).forEach(year -> IntStream.rangeClosed(1, 12)
                 .forEach(month -> {
                     String curUrl = MENU_SITE_PRE + year + "&quarter_id=" + month;
                         // + (month < 10 ? "0" + month : month);
